a8dffeae1282016d083d13bc6bb4df85869e2d61
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-11-09 17:56:48 +0200

    datatypes 1`

---
+++ 1-intro/hw/Datatypes.java
@@ -0,0 +1,36 @@
+
+public class Datatypes {
+
+    public static void main(String... args) {
+
+        boolean isActive = false;
+        
+        // 1 byte
+        byte b = 4; 
+
+        // 2 bytes
+        short s = 344;
+
+        // 4 bytes
+        int i = 33333;
+
+        // 8 bytes
+        long l = 2342222;
+
+        // 4 bytes
+        float f = 333.65F;
+
+        // 8 bytes
+        double d = 2.4;
+
+        // 2 bytes
+        char ch = 200;
+
+    }
+}
+
+class IntTypes {
+
+    
+}
+
