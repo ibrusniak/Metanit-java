4e9133eb8d568e15145ffe7c44c947a8b38951de
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-11-11 17:04:56 +0200

    use `static import`

---
+++ 2-OOP/ImportStatic.java
@@ -0,0 +1,10 @@
+
+import static utils.Utils.*;
+
+public class ImportStatic {
+    
+    public static void main(String[] args) {
+        
+        prn("hello");
+    }
+}
---
+++ 2-OOP/utils/Utils.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Utils {
+    
+
+    public static void prn(Object o) {
+
+        System.out.println(o);
+    }
+}
