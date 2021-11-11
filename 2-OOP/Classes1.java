08f2b4e428439855a25fcd4a4e2da63bf9c990db
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-11-11 11:06:50 +0200

    use `Class` and so on...

---
+++ 2-OOP/Classes1.java
@@ -0,0 +1,19 @@
+
+public class Classes1 {
+    
+    public static void main(String[] args) {
+        
+        MyClass1 m = new MyClass1();
+
+        System.out.println(m.getClass());
+        System.out.println(m.hashCode());
+
+        
+    }
+}
+
+class MyClass1 {
+
+    
+}
+
