613be79f0999f29e4d555df8cd99ea40e57777a7
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-11-12 17:08:38 +0200

    create *.jar executable app

---
+++ 01-basics/FirstApp/Program.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Java!");
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        scanner.close();
+    }
+}
---
+++ 01-basics/FirstApp/readme.txt
@@ -0,0 +1,9 @@
+
+How to create simple executable java app
+
+1. Create main *.java file
+2. Compile *.java file with javac
+3. Create manifest file
+4. Use "jar -c -m manifest.mf -f prog.jar Program.class" to generate jar file
+5. Run jar file with 'java -jar prog.jar'
+
