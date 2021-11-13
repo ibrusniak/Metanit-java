e662c98aaf472b93e4a56867d649652d60b30be5
ibrusniak <ivan.brusniak@gmail.com>
2021-11-13 08:59:52 +0200

    use constructor to create class instanse

---
+++ 03-OOP/Constructors/src/ConstructorsTest1.java
@@ -0,0 +1,13 @@
+
+
+public class ConstructorsTest1 {
+
+    public static void main(String[] args) {
+        
+        new SomeClass();
+        new SomeClass(10);
+        new SomeClass("Jack");
+        new SomeClass(22, "Arnold");
+    }
+}
+
