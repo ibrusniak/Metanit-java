c7222efd6acbb94a1ebd3d372a2c0fda32677bd5
ibrusniak <ivan.brusniak@gmail.com>
2021-12-18 18:24:07 +0200

    use methods

---
+++ 2-basics/methods/MethodsDemo.java
@@ -0,0 +1,19 @@
+
+public class MethodsDemo {
+
+    public static void main(String[] args) {
+        
+        doSmth();
+    }
+
+    public static void doSmth() {
+        
+        print("doSmth");
+    }
+
+    public static void print(Object o) {
+        
+        System.out.println(o);
+    }
+}
+
---
+++ 2-basics/methods/OverloadingDemo.java
@@ -0,0 +1,26 @@
+
+/**
+ * OverloadingDemo
+ */
+public class OverloadingDemo {
+
+    public static void main(String[] args) {
+        
+
+    }
+
+    public static int sum(int a, int b) {
+
+        return a + b;
+    }
+
+    public static float sum(float a, float b) {
+
+        return a + b;
+    }
+
+    public static double sum(double a, double b) {
+
+        return a + b;
+    }
+}
