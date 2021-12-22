05ae7ab5205b41a95a32195e98769d563b12a453
ibrusniak <ivan.brusniak@gmail.com>
2021-12-22 06:54:04 +0200

    may be like this

--- 3-OOP/inner-classes/InnerClasses3/Main.java
+++ 3-OOP/inner-classes/InnerClasses3/Main.java
@@ -3,5 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
         
+        Table t1 = new Table();
+        Table.Header header1 = t1.new Header();
+        header1.addColumnt(header1.new Column("age", Table.Header.Column.typeOfData.INT));
     }
 }
--- 3-OOP/inner-classes/InnerClasses3/Table.java
+++ 3-OOP/inner-classes/InnerClasses3/Table.java
@@ -14,34 +14,44 @@ public class Table {
         
         private Column[] columns;
 
-        public Header(Column[] columns) {
-            this.columns = columns;
-        }
-    }
+        public void addColumnt(Column column) {
 
-    public class Column {
+            if(columns == null) {
+                columns = new Column[] {column};
+            } else {
+                Column[] newColumns = new Column[columns.length + 1];
+                for(int i = 0; i < columns.length; i++) {
+                    newColumns[i] = columns[i];
+                }
+                newColumns[newColumns.length - 1] = column;
+                columns = newColumns;
+            }
+        }
+
+        public class Column {
         
-        public enum typeOfData {
-            INT,
-            VARCHAR,
-            FLOAT
-        }
-
-        private String name;
-        private typeOfData typeOfData;
-
-        public Column(String name, typeOfData typeOfData) {
-            
-            this.name = name;
-            this.typeOfData = typeOfData;
-        }
-
-        public typeOfData gettypeOfData() {
-            return this.typeOfData;
-        }
-
-        public String getName() {
-            return this.name;
+            public enum typeOfData {
+                INT,
+                VARCHAR,
+                FLOAT
+            }
+    
+            private String name;
+            private typeOfData typeOfData;
+    
+            public Column(String name, typeOfData typeOfData) {
+                
+                this.name = name;
+                this.typeOfData = typeOfData;
+            }
+    
+            public typeOfData gettypeOfData() {
+                return this.typeOfData;
+            }
+    
+            public String getName() {
+                return this.name;
+            }
         }
     }
 }
