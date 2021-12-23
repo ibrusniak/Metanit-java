6431109879da3c3d56c26eb1c233bf661515b5f3
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-12-23 17:29:52 +0200

    Done some practice. Wrote classes and methods stub

---
+++ 0-practice/Calculator/src/main/App.java
@@ -0,0 +1,14 @@
+
+package main;
+
+import static utils.ConsoleOutput.*;
+
+/**
+ * App - main class of Calculator project
+ */
+public class App {
+
+    public static void main(String[] args) {
+        print("hello");
+    }
+}
---
+++ 0-practice/Calculator/src/utils/ConsoleOutput.java
@@ -0,0 +1,14 @@
+
+package utils;
+
+public class ConsoleOutput {
+
+    public static <T> void print(T object) {
+        System.out.print(o);
+    }
+
+    public static <T> void println(T object) {
+        System.out.println(o);
+    }
+}
+
