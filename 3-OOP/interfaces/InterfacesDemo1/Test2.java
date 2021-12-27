877083deae42bb9eafedd05419bd569ba6952da3
ibrusniak <ivan.brusniak@gmail.com>
2021-12-27 11:39:20 +0200

    Use access to interface`s constants with interface`s name

---
+++ 3-OOP/interfaces/InterfacesDemo1/Test2.java
@@ -0,0 +1,27 @@
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        
+        // Use simple access (through interface name)
+        // to static fields of an interface
+        toConsole(Interface1.x);
+        toConsole(Interface1.y);
+    }
+
+    public static void toConsole(Object o) {
+        System.out.printf("[%s]: %s\n", o.getClass().getSimpleName(), o);
+    }
+}
+
+interface Interface1 {
+
+    int x = 100;
+    public final int y = 200;
+}
+
+class Class1 implements Interface1 {
+
+
+}
+
