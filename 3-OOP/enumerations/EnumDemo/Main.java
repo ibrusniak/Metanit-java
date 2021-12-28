c25ea64edcf5a970c7e5a18887806d2ef3a12643
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-12-28 16:05:32 +0200

    Use constructors and members into enum class

--- 3-OOP/enumerations/EnumDemo/Main.java
+++ 3-OOP/enumerations/EnumDemo/Main.java
@@ -9,6 +9,10 @@ public class Main {
         System.out.println();
         System.out.println(Day.MONDAY.ordinal());
         System.out.println(Day.values()[Day.values().length - 1]);
+
+        for(MyEnumeration1 m : MyEnumeration1.values()) {
+            System.out.println(m.getRep());
+        }
     }
 }
 
@@ -20,4 +24,23 @@ enum Day {
     FRIDAY,
     SATURDAY,
     SUNDAY
+}
+
+enum MyEnumeration1 {
+
+    FIRST_CONSTANT("Representation of the first constant"),
+    SECOND_CONSTANT("Representation of the second constant");
+
+    private String representation;
+
+    // Private (only) constructor
+    // can be "instantiated" only from body
+    // of emumeration's class
+    private  MyEnumeration1(String representation) {
+        this.representation = representation;
+    }
+
+    public String getRep() {
+        return this.representation;
+    }
 }
