f35652bf82bb8b4c7836af17be44e37ec18ec90a
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-12-28 17:28:12 +0200

    Override toString() and hashCode()

--- 3-OOP/class-object/ObjectDemo1/Main.java
+++ 3-OOP/class-object/ObjectDemo1/Main.java
@@ -15,11 +15,22 @@ public class Main {
         prn(obj.equals(obj2));
         prn(!obj.equals(obj2));
 
-        
+        prn(new Main().toString());
+        prn(new Main().hashCode());
     }
 
     public static void prn(Object o) {
         System.out.println(o);
+    }
+
+    @Override
+    public String toString() {
+        return "Main class instance " + hashCode();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode() * 15;
     }
 }
 
