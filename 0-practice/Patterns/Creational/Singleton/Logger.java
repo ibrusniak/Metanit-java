7a7df31461d37455fbfe69cc7ec993f33f6848ac
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-12-28 11:06:46 +0200

    Use Singleton pattern to create Logger instance

---
+++ 0-practice/Patterns/Creational/Singleton/Logger.java
@@ -0,0 +1,24 @@
+
+public class Logger {
+
+    public static void main(String[] args) {
+        
+        Logger logger = Logger.getLogger();
+    }
+}
+
+final class Logger {
+
+    private Logger() {};
+
+    private Logger logger;
+
+    public Logger getLogger() {
+
+        if(logger == null) {
+            logger = new Logger();
+        }
+        return logger;
+    }
+}
+
