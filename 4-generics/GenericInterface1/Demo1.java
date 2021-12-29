fe7c026a8596f11b5ee4ee02d98de4f1fab1f2bc
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-12-29 14:24:30 +0200

    Use genric interface. Case 2

--- 4-generics/GenericInterface1/Demo1.java
+++ 4-generics/GenericInterface1/Demo1.java
@@ -5,6 +5,9 @@ public class Demo1 {
         
         SomeClass1 sc = new SomeClass1();
         System.out.println(sc.getT());
+
+        SomeClass2<Float> sc2 = new SomeClass2<Float>(2.2f);
+        System.out.println(sc2.getT());
     }
 }
 
@@ -21,5 +24,18 @@ class SomeClass1 implements Interface1<String> {
     }
 }
 
+class SomeClass2<T> implements Interface1<T> {
+
+    T t;
+
+    public SomeClass2(T t) {
+        this.t = t;
+    }
+
+    @Override
+    public T getT() {
+        return t;
+    }
+}
 
 
