59d90a5dafdb11bf0623f4f53a75d8b87500bbcd
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-12-29 11:54:30 +0200

    Use generics. 1

--- 4-generics/GenericsDemo1/Main.java
+++ 4-generics/GenericsDemo1/Main.java
@@ -18,6 +18,14 @@ public class Main {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
+        SomeClass2<Integer> object3 = new SomeClass2<Integer>(100);
+        SomeClass2<Float> object4 = new SomeClass2<Float>(200.344f);
+        SomeClass2<String> object5 = new SomeClass2<String>("ident_444");
+
+        System.out.println(object3.getId());
+        System.out.println(object4.getId());
+        System.out.println(object5.getId());
     }
 }
 
@@ -31,6 +39,19 @@ class SomeClass1 {
     
     public Object getIdentifier() {
         return identifier;
+    }
+}
+
+class SomeClass2<T> {
+
+    private T id;
+
+    SomeClass2(T id) {
+        this.id = id;
+    }
+
+    public T getId() {
+        return this.id;
     }
 }
 
