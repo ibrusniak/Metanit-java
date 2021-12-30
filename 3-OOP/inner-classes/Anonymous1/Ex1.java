c88885a7adf84af0caaa2cc07c579ad7c0ac0615
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-12-30 14:33:53 +0200

    Use anonymous class. Implement an Interface

--- 3-OOP/inner-classes/Anonymous1/Ex1.java
+++ 3-OOP/inner-classes/Anonymous1/Ex1.java
@@ -3,6 +3,8 @@ public class Ex1 {
 
     public static void main(String[] args) {
 
+        // We instantiate an anonymous class
+        // from an existent one
         doSomeThing(new Object(){
 
             @Override
@@ -10,12 +12,32 @@ public class Ex1 {
                 return "Anonymous class instance " + hashCode();
             }
         });
+
+        // Implement an interface
+        IFace d = new IFace(){
+
+            @Override
+            public String toString() {
+                return "Anonymous 2 " + hashCode();
+            }
+
+            @Override
+            public String doSomeThing() {
+                return "hello";
+            }
+        };
+
+        System.out.println(d);
     }
 
     public static void doSomeThing(Object o) {
-        System.out.printf("%s %s",
+        System.out.printf("%s %s\n\n",
             o.getClass().getName(),
             o);
     }
+}
+
+interface IFace {
+    public String doSomeThing();
 }
 
