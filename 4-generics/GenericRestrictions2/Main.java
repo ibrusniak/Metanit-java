701a5a0777afd789b156f91ddbb9a32c9baa447c
Ivan Brusniak <ivan.brusniak@gmail.com>
2022-01-04 11:16:43 +0200

    Use `double generic restriction`

---
+++ 4-generics/GenericRestrictions2/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
+
+class Class1<Type extends Number> {}
+
+class Class2<Type extends Class1<Integer>> {}
+
+
+
+
