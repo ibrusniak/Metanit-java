ba9fcf9d60644bd6c54a728370ba9a80e2b918ca
ibrusniak <ivan.brusniak@gmail.com>
2022-01-08 13:46:10 +0200

    Use `uncheked` exceptions. Add descrioption

--- 5-exceptions/Ex1/Main.java
+++ 5-exceptions/Ex1/Main.java
@@ -1,6 +1,13 @@
 
 /**
- * Main
+ * Demonstrates `unchecked exceptions`. You don't need to
+ * use 'try-catch clause' even if you call method that
+ * declared with 'throws stmt'...
+ * 
+ * Unhandled are all exceptions:
+ *  Object -> Throwable -> Exception -> Exception -> RuntimeException
+ *  -> ... (all derived from RuntimeException)
+ * 
  */
 public class Main {
 
