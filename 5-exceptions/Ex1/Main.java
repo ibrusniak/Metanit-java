d626bb1f31018c062d7a359cf7ea5fa6ecfbd9c8
ibrusniak <ivan.brusniak@gmail.com>
2022-01-08 13:45:31 +0200

    Use `uncheked` exceptions

---
+++ 5-exceptions/Ex1/Main.java
@@ -0,0 +1,19 @@
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        throwSome();
+    }
+
+    public static void throwSome() throws RuntimeException {
+
+        throw new RuntimeException();
+    }
+}
+
+
+
