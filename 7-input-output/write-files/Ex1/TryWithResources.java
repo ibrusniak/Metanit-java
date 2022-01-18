c68aefa36376b77a9d08b5b442d0805c1dbaab14
Ivan Brusniak <ivan.brusniak@gmail.com>
2022-01-18 11:02:03 +0200

    Create file into filesystem

--- 7-input-output/write-files/Ex1/TryWithResources.java
+++ 7-input-output/write-files/Ex1/TryWithResources.java
@@ -1,19 +1,18 @@
+import java.io.File;
+import java.util.Random;
 
 public class TryWithResources {
 
     public static void main(String[] args) {
         
-        Random rnd1 = new Random();
-        Random rnd2 = new Random();
+        Random rnd = new Random();
+        String fileName = String.format("testFile%s", rnd.nextInt(1025));
+        File file = new File(fileName);
 
-        for(int i = 0; i < 10; i++) {
-            System.out.println("random: " + rnd1.nextInt(16));
-        }
-
-        System.out.println();
-
-        for(int i = 0; i < 10; i++) {
-            System.out.println("random10: " + rnd2.nextInt(30));
+        try {
+            file.createNewFile();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
