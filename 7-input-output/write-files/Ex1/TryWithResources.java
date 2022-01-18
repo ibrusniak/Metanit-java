13fbc0a4c41417b6386fbae1c862553ae84511c4
Ivan Brusniak <ivan.brusniak@gmail.com>
2022-01-18 11:57:58 +0200

    Use DataInputStream to read lines from console

--- 7-input-output/write-files/Ex1/TryWithResources.java
+++ 7-input-output/write-files/Ex1/TryWithResources.java
@@ -1,4 +1,6 @@
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Random;
 
 public class TryWithResources {
@@ -13,7 +15,21 @@ public class TryWithResources {
             file.createNewFile();
         } catch (Exception e) {
             e.printStackTrace();
+            return;
         }
+
+        FileOutputStream fos = new FileOutputStream(file);
+        DataInputStream dis = new DataInputStream(System.in);
+        
+        String input = "";
+
+        while (input != "halt") {
+
+            input = dis.readUTF();
+        }
+
+        fos.close();
+        dis.close();
     }
 }
 
