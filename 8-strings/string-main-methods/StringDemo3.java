ca3f9e3f20e4507a5cb500ea9668831ffcf5b058
ibrusniak <ivan.brusniak@gmail.com>
2022-01-29 12:05:32 +0200

    Use replaceAll

--- 8-strings/string-main-methods/StringDemo3.java
+++ 8-strings/string-main-methods/StringDemo3.java
@@ -24,15 +24,20 @@ public class StringDemo3 {
 
         try {
             
-            Stirng initialString =
+            String initialString =
                 """
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
                 """;
 
             System.out.println("Initial string = \n\n" + initialString);
+            
+            System.out.println();
+            
+            System.out.println("replaced whitespaces = \n\n" + initialString.replaceAll("\s", "\t"));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
