cee39bee50b01503d18e944af143d70d41baad37
ibrusniak <ivan.brusniak@gmail.com>
2022-01-29 12:00:31 +0200

    Use replaceAll. Prepare

--- 8-strings/string-main-methods/StringDemo3.java
+++ 8-strings/string-main-methods/StringDemo3.java
@@ -19,6 +19,23 @@ public class StringDemo3 {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        System.out.println();
+
+        try {
+            
+            Stirng initialString =
+                """
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                """;
+
+            System.out.println("Initial string = \n\n" + initialString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
