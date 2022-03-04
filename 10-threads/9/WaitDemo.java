852a3a760567e80d760a0a0c4a974cb87a40c867
ibrusniak <ivan.brusniak@gmail.com>
2022-03-04 10:11:29 +0200

    Use `wait`. 2

--- 10-threads/9/WaitDemo.java
+++ 10-threads/9/WaitDemo.java
@@ -1,14 +1,46 @@
 
-import java.util.function.Function;
+import java.util.function.Consumer;
 
 public class WaitDemo {
 
     public static void main(String[] args) {
 
-        Function<String, Integer> cons = (n) -> n.length();
-        System.out.println(cons.apply("hello"));
+        Worker w = new Worker(new Object());
+        w.start();
+
+        Thread ct = Thread.currentThread();
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println(w.getName() + ", " + w.isAlive() + ", " + w.isInterrupted());
+            try {
+                ct.sleep(1200);
+            } catch (InterruptedException e) {}
+        }
     }
 }
 
+class Worker extends Thread {
 
+    private Object o;
+
+    public Worker(Object o) {
+        this.o = o;
+    }
+
+    @Override
+    public void run() {
+
+        synchronized (o) {
+
+            try {
+                // this is cause of
+                // waiting forever
+                o.wait(12000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
 
