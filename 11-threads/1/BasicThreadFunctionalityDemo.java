aaa5723e3a22a62dc6c8570fa1f8acfcbc8bf4c0
ibrusniak <ivan.brusniak@gmail.com>
2022-04-15 20:58:42 +0300

    Use `setName()` method

--- 11-threads/1/BasicThreadFunctionalityDemo.java
+++ 11-threads/1/BasicThreadFunctionalityDemo.java
@@ -4,6 +4,11 @@ public class BasicThreadFunctionalityDemo {
     public static void main(String[] args) {
 
         new UThread().start();
+        
+        Thread t0 = new UThread();
+        System.out.println(t0);
+        t0.setName("myThread0");
+        System.out.println(t0);
     }
 }
 
