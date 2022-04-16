395c0073cea147d4614add53b0fd31cdce53fa46
ibrusniak <ivan.brusniak@gmail.com>
2022-04-16 08:25:25 +0300

    Show how to use `Runnable` iface to create thread

--- 11-threads/1/ThreadCreateDemo.java
+++ 11-threads/1/ThreadCreateDemo.java
@@ -6,7 +6,7 @@ public class ThreadCreateDemo {
         Thread mainThread = Thread.currentThread();
         System.out.println(mainThread.getName() + " started");
         new JThread("My happy thread").start();
-        new FThread("Runnable thread").start();
+        new Thread(new FThread(), "Runnable thread").start();
         System.out.println(mainThread.getName() + " ended");
     }
 }
@@ -29,15 +29,11 @@ class JThread extends Thread {
 
 class FThread implements Runnable {
 
-    public FThread(String threadName) {
-        Thread.currentThread().setName(threadName);
-    }
-
     public void run() {
         Thread ct = Thread.currentThread();
         System.out.println(ct.getName() + " started");
         try {
-            sleep(2000);
+            ct.sleep(2000);
         } catch (InterruptedException e) {}
         System.out.println(ct.getName() + " ended");
     }
