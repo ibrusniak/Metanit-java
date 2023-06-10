5f5fa4f45a5a9ed539c181dee3a4af4ec3430fc8
Ivan Brusniak <ivan.brusniak@gmail.com>
2023-06-10 09:59:42 +0300

    Use initializer, static initialize and constructor. Example 1

---
+++ Ch3/Example1/MyClass1.java
@@ -0,0 +1,17 @@
+public class MyClass1 {
+
+    static {
+        System.out.println("static initializer.");
+    }
+    {
+        System.out.println("initializer.");
+    }
+    MyClass1() {
+        System.out.println("constructor \"()\"");
+    }
+    public static void main(String[] args) {
+        new MyClass1();
+        new MyClass1();
+        new MyClass1();
+    }
+}
