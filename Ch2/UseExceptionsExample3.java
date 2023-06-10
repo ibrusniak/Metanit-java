a40e12922358c0a8e486cdf26b4b919ba3815f50
Ivan Brusniak <ivan.brusniak@gmail.com>
2023-06-10 09:26:58 +0300

    Use exceptions. Example 3

---
+++ Ch2/UseExceptionsExample3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class UseExceptionsExample3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = "";
+        do {
+            System.out.print("Come on, try some: ");
+            s = scanner.nextLine();
+            try {
+                int a = Integer.parseInt(s);
+            } catch (java.util.UnknownFormatConversionException e) {
+                System.out.printf("A. Exception \"%1\"", e.getMessage());
+            } catch (java.lang.NumberFormatException e) {
+                System.out.printf("B. Exception \"%1\"", e.getMessage());
+            }
+        } while (!s.equals("exit"));
+        scanner.close();
+    }
+}
